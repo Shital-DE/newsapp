6dc597e1169f94458b42c886fde220a037b527f7
Shital-DE <dattaentind@gmail.com>
2025-01-04 11:41:49 +0530

    New api integration. Next and previous buttons.

--- src/components/News.js
+++ src/components/News.js
@@ -2,69 +2,70 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles =  [
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": null,
-            "title": "Sam Konstas: Teenage Australia opener's journey to India Test in Melbourne",
-            "description": "Matching Ricky Ponting and beating Steve Smith, those close to Sam Konstas tell BBC Sport about his journey to international cricket.",
-            "url": "http://www.bbc.co.uk/sport/cricket/articles/c8xjw8v99kdo",
-            "urlToImage": "https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/6f22/live/bcad0710-c5ea-11ef-a2ca-e99d0c9a24e3.jpg",
-            "publishedAt": "2024-12-31T08:37:16.9676301Z",
-            "content": "Konstas may have failed with those first two scoops, but he tried again soon after and hit Bumrah - the world's top-ranked bowler - for a six and two fours on the way to 60 from 65 balls.\r\nThose shot… [+2705 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
 
    constructor() {
       super();
      this.state = {
-       articles: this.articles,
-       loading: false
+       articles: [],
+       loading: true,
+       page: 1,
      }
   }
   
+  async componentDidMount()  { 
+    let url = 'https://newsapi.org/v2/everything?q=from=2025-01-03&to=2025-01-03&apiKey=7200102f1f7440c48a9d8b88cacef6c7&pageSize=9&page=1';
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles, loading: false,  page:1, totalResults: parsedData.totalResults});
+  }
+
+  handlePreviousClick = async () => {
+     let url = `https://newsapi.org/v2/everything?q=from=2025-01-03&to=2025-01-03&apiKey=7200102f1f7440c48a9d8b88cacef6c7&pageSize=9&page=${this.state.page-1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles: parsedData.articles, loading: false, page: this.state.page-1,});
+   }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.totalResults / 12)) {
+      
+    } else {
+      let url = `https://newsapi.org/v2/everything?q=from=2025-01-03&to=2025-01-03&apiKey=7200102f1f7440c48a9d8b88cacef6c7&pageSize=9&page=${this.state.page + 1}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({ articles: parsedData.articles, loading: false, page: this.state.page + 1, });
+    }
+   }
+
   render() {
     return (
       <div className='container my-3'>
-        <h2>Top Headlines</h2>  
+        <h1 className="text-center">Top Headlines</h1>
+        
         <div className="row">
-          {this.state.articles.map((element) => {
-              return <div className="col-md-4" key={element.url}>
-                        <NewsItem  title={element.title.slice(0,45)} description={element.description.slice(0, 88)}
-                          imageUrl={element.urlToImage} newsUrl={element.url} />
-                      </div>
-                    
-          })}
+          
+         {this.state.articles !== undefined ? (
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title == null ? '' : element.title.slice(0, 45)}
+                    description={element.description == null ? '' : element.description.slice(0, 88)}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <p>No articles available</p> // Fallback when articles are empty
+          )}
+
         </div>  
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Revious</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.totalResults / 12)} type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,15 @@ export class NewsItem extends Component {
     return (
         <div className='my-3'>
             <div className="card" style={{ width: "18rem"}}>
-                <img className="card-img-top" src={imageUrl} alt="Card image cap"/>
-                <div className="card-body">
+            <img
+              className="card-img-top"
+              src={imageUrl == null ? 'https://plus.unsplash.com/premium_photo-1707080369554-359143c6aa0b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D' : imageUrl}
+              alt="News card"
+            />
+            <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
-                    <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                    <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
                 </div>
             </div>
          </div>
