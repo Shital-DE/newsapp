ede5de42372ddc4bcad845a80e0759cd810edd28
Shital-DE <dattaentind@gmail.com>
2025-01-09 10:40:59 +0530

    Changing class based components to function based components.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,67 @@
-import React, { Component } from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-export class Navbar extends Component {
-
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/">News App</NavLink>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">
+          News App
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-                <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
+              <NavLink className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/business">Business</NavLink>
+              <NavLink className="nav-link" to="/business">
+                Business
+              </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/entertainment">Entertainment</NavLink>
+              <NavLink className="nav-link" to="/entertainment">
+                Entertainment
+              </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/health">Health</NavLink>
+              <NavLink className="nav-link" to="/health">
+                Health
+              </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/science">Science</NavLink>
+              <NavLink className="nav-link" to="/science">
+                Science
+              </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/sports">Sports</NavLink>
+              <NavLink className="nav-link" to="/sports">
+                Sports
+              </NavLink>
             </li>
             <li className="nav-item">
-                <NavLink className="nav-link" to="/technology">Technology</NavLink>
+              <NavLink className="nav-link" to="/technology">
+                Technology
+              </NavLink>
             </li>
-            </ul>
+          </ul>
         </div>
-        </nav>
-      </div>
-    )
-  }
-}
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/News.js
+++ src/components/News.js
@@ -1,59 +1,40 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "us",
-    pageSize: 9,
-    category: "general",
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLeading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  // document.title = `News - ${this.capitalizaString(props.category)}`;
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    };
-    document.title = `News - ${this.capitalizaString(this.props.category)}`;
-  }
-
-  capitalizaString(string) {
+  const capitalizaString = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
 
-  async updatedNews(page) {
-    this.props.setProgress(0);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7200102f1f7440c48a9d8b88cacef6c7&pageSize=${this.props.pageSize}&page=${page}`;
-    this.props.setProgress(25);
-    this.setState({ loading: true });
-    this.props.setProgress(50);
+  const updatedNews = async (page) => {
+    props.setProgress(0);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7200102f1f7440c48a9d8b88cacef6c7&pageSize=${props.pageSize}&page=${page}`;
+    props.setProgress(25);
+    setLeading(true);
+    props.setProgress(50);
     let data = await fetch(url);
-    this.props.setProgress(75);
+    props.setProgress(75);
     let parsedData = await data.json();
-    this.props.setProgress(90);
-    this.setState({
-      articles: parsedData.articles,
-      loading: false,
-      page: page,
-      totalResults: parsedData.totalResults,
-    });
-    this.props.setProgress(100);
-  }
+    props.setProgress(90);
+    setArticles(parsedData.articles);
+    setLeading(false);
+    setPage(page);
+    setTotalResults(parsedData.totalResults);
+    props.setProgress(100);
+  };
 
-  async componentDidMount() {
-    this.updatedNews(1);
-  }
+  useEffect(() => {
+    updatedNews(1);
+  }, []);
 
   // handlePreviousClick = async () => {
   //   this.updatedNews(this.state.page - 1 );
@@ -63,93 +44,103 @@ export class News extends Component {
   //   this.updatedNews(this.state.page + 1);
   // }
 
-  fetchMoreData = async () => {
+  const fetchMoreData = async () => {
     const url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${
-      this.props.pageSize
-    }&page=${this.state.page + 1}`;
+      props.country
+    }&category=${props.category}&apiKey=${props.apiKey}&pageSize=${
+      props.pageSize
+    }&page=${page + 1}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      loading: false,
-      page: this.state.page + 1,
-      totalResults: parsedData.totalResults,
-    });
+    setArticles(parsedData.articles);
+    setLeading(false);
+    setPage(page);
+    setTotalResults(parsedData.totalResults);
+    // this.setState({
+    //   articles: this.state.articles.concat(parsedData.articles),
+    //   loading: false,
+    //   page: this.state.page + 1,
+    //   totalResults: parsedData.totalResults,
+    // });
   };
 
-  render() {
-    return (
-      <div className="container my-3">
-        <h1 className="text-center" style={{ margin: "15px 0px" }}>
-          Top {this.capitalizaString(this.props.category)} Headlines
-        </h1>
-        {this.state.loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={
-            this.state.articles === undefined ? 0 : this.state.articles.length
-          }
-          next={this.fetchMoreData}
-          hasMore={
-            this.state.articles !== undefined
-              ? this.state.articles.length !== this.state.totalResults
-              : false
-          }
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {this.state.articles && this.state.articles.length > 0 ? (
-                this.state.articles.map((element) => {
-                  return (
-                    <div className="col-md-4" key={element.url}>
-                      <NewsItem
-                        title={
-                          element.title == null
-                            ? ""
-                            : element.title.slice(0, 45)
-                        }
-                        description={
-                          element.description == null
-                            ? ""
-                            : element.description.slice(0, 88)
-                        }
-                        imageUrl={element.urlToImage}
-                        newsUrl={element.url}
-                        author={
-                          element.author == null ? "Unknown" : element.author
-                        }
-                        date={element.publishedAt}
-                        source={element.source.name}
-                      />
-                    </div>
-                  );
-                })
-              ) : (
-                <div
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    display: "flex",
-                    height: "10vh",
-                    width: "100vw",
-                  }}
-                >
-                  <p style={{ textAlign: "center" }}>No articles available</p>
-                </div>
-              )}
-            </div>
+  // render() {
+  return (
+    <div className="container my-3">
+      <h1 className="text-center" style={{ margin: "15px 0px" }}>
+        Top {capitalizaString(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles === undefined ? 0 : articles.length}
+        next={fetchMoreData}
+        hasMore={
+          articles !== undefined ? articles.length !== totalResults : false
+        }
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles && articles.length > 0 ? (
+              articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={
+                        element.title == null ? "" : element.title.slice(0, 45)
+                      }
+                      description={
+                        element.description == null
+                          ? ""
+                          : element.description.slice(0, 88)
+                      }
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={
+                        element.author == null ? "Unknown" : element.author
+                      }
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })
+            ) : (
+              <div
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  display: "flex",
+                  height: "10vh",
+                  width: "100vw",
+                }}
+              >
+                <p style={{ textAlign: "center" }}>No articles available</p>
+              </div>
+            )}
           </div>
-        </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between">
+        </div>
+      </InfiniteScroll>
+      {/* <div className="container d-flex justify-content-between">
           <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Revious</button>
-          <button disabled={(this.state.page > 1 && this.state.articles === undefined)? true :  this.state.page + 1 > Math.ceil(this.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
+          <button disabled={(this.state.page > 1 && this.state.articles === undefined)? true :  this.state.page + 1 > Math.ceil(this.totalResults / props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
         </div> */}
-      </div>
-    );
-  }
-}
+    </div>
+  );
+  // }
+};
+
+News.defaultProps = {
+  country: "us",
+  pageSize: 9,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
